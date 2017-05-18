abdab518e09aadfc554c69b9513ce83b932f719e
Julio Henriquez <julio.m.henriquez@gmail.com>
2017-05-18 02:58:03 -0400

    implements user service and messages initial state

--- src/root.jsx
+++ src/root.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 
 import Publish     from './components/publish';
 import MessageList from './components/messageList';
+import UserService from './services/userService';
 
 
 class Container extends React.Component {
@@ -11,10 +12,16 @@ class Container extends React.Component {
   constructor (props) {
     super(props);
 
+    this.userService = new UserService();
+
     this.state = {
       auth: {
         isLoading: true,
         user: null
+      },
+      messages: {
+        isLoading: true,
+        list: []
       }
     };
   }
@@ -24,14 +31,24 @@ class Container extends React.Component {
       <div id="container">
         <div id="content">
           <Publish auth={ this.state.auth } />
-          <MessageList />
+          <MessageList messages={ this.state.messages } />
         </div>
       </div>
     );
   }
 
   componentDidMount() {
+    this.userService
+        .getCurrentUser()
+        .then((rs) => {
 
+          if (rs.status === 200) {
+            this.setState({ auth: { user: rs.body } });
+          }
+
+          this.setState({ auth: { isLoading: false } });
+
+        });
   }
 
 }
