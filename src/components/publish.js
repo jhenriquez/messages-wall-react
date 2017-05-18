d1774bf6926f1891e3fb947047f1b8af158a4fa9
Julio Henriquez <julio.m.henriquez@gmail.com>
2017-05-17 22:45:44 -0400

    Implements loading and auth prop on publish component

--- src/components/publish.js
+++ src/components/publish.js
@@ -1,8 +1,14 @@
 import React    from 'react';
 import ReactDom from 'react-dom';
 
+import Loading from './loading';
+
 export default class Publish extends React.Component {
   render() {
-    return <h1>Publish Here!</h1>;
+    return this.props.auth.isLoading ?
+      <Loading message="Authenticating!" /> :
+      (<div>
+        <h1>Publish Here!</h1>
+      </div>);
   }
 }
