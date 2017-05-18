0677c60cfe1934e43559a79e233223c71789fb6b
Julio Henriquez <julio.m.henriquez@gmail.com>
2017-05-17 22:45:26 -0400

    Adds initial state for authentication.

--- src/root.jsx
+++ src/root.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React     from 'react';
+import ReactDOM  from 'react-dom';
 import PropTypes from 'prop-types';
 
 import Publish     from './components/publish';
 import MessageList from './components/messageList';
 
+
 class Container extends React.Component {
+
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      auth: {
+        isLoading: true,
+        user: null
+      }
+    };
+  }
+
   render() {
     return (
       <div id="container">
         <div id="content">
-          <Publish />
+          <Publish auth={ this.state.auth } />
           <MessageList />
         </div>
       </div>
     );
   }
+
+  componentDidMount() {
+
+  }
+
 }
 
 ReactDOM.render(
