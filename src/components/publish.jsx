bf6aec7bb7ccf03e03f9130f5cefb6753f0c95d1
Julio Henriquez <julio.m.henriquez@gmail.com>
2017-06-03 13:09:33 -0400

    Refactors whenAnonymous jsx into AnonymousPublisher internal component.

--- src/components/publish.jsx
+++ src/components/publish.jsx
@@ -1,19 +1,24 @@
 import React from 'react';
 
+export class AnonymousPublisher extends React.Component {
+  render() {
+    return (
+        <div className="anonymous-content">
+          <p className="message">
+            You need to either <a>signup</a> or <a>signin</a> before you can post any content.
+          </p>
+        </div>
+    );
+  }
+}
+
 export default class Publish extends React.Component {
 
   render() {
-    const whenAnonymous = (
-      <div className="anonymous-content">
-        <p className="message">
-          You need to either <a>signup</a> or <a>signin</a> before you can post any content.
-        </p>
-      </div>
-    );
 
     return (
       <div className="publish-content">
-        { this.props.user ? null : whenAnonymous }
+        { this.props.user ? null : <AnonymousPublisher /> }
       </div>
     );
   }
--- src/components/publish.spec.jsx
+++ src/components/publish.spec.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
 import { shallow } from 'enzyme';
 
-import Publish from './publish';
+import Publish, { AnonymousPublisher } from './publish';
 
 describe('<Publish />', () => {
   describe('anonymous (no-user provided)', () => {
 
-    test('It renders an ".anonymous-content" element', () => {
-      const anonymousContent = shallow(<Publish />).find('.anonymous-content');
+    test('It renders an <AnonymousPublisher /> element', () => {
+      const anonymousContent = shallow(<Publish />).find(AnonymousPublisher);
       expect(anonymousContent).toHaveLength(1);
     });
 
     test('It should indicate the user needs to "signup"', () => {
-      const anonymousContent = shallow(<Publish />).find('.anonymous-content');
+      const anonymousContent = shallow(<Publish />);
       expect(anonymousContent.html()).toEqual(expect.stringContaining('signup'));
     });
 
     test('It should indicate the user needs to "signin"', () => {
-      const anonymousContent = shallow(<Publish />).find('.anonymous-content');
+      const anonymousContent = shallow(<Publish />);
       expect(anonymousContent.html()).toEqual(expect.stringContaining('signin'));
     });
 
