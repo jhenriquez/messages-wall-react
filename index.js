7c674943c1a5bd8fb0b664c18e462354ed62c934
Julio Henriquez <julio.m.henriquez@gmail.com>
2017-06-14 02:16:52 -0400

    Removes ES6 module imports from server entry point.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-import dotenv from 'dotenv';
-import server from 'messages-wall-api';
+require('dotenv').config();
 
-dotenv.config();
+const server = require('messages-wall-api');
 
 server.listen(process.env.PORT || 8180, () => {
   console.log(`listening on :8180`);
