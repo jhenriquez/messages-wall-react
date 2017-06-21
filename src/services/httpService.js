a41d2302cb53aadee5f780dd6dad77da4d213744
Julio Henriquez <julio.m.henriquez@gmail.com>
2017-06-21 15:59:12 -0400

    Adds plugin to have promises reject bad status responses

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,11 +1,12 @@
 import * as popsicle from 'popsicle';
+import status from 'popsicle-status';
 
 export default class HttpService {
   get(url: string): Promise<any> {
-    return popsicle.get(url);
+    return popsicle.get(url).use(status());
   }
 
   post(url: string, data: any): Promise<any> {
-    return popsicle.post(url, data).use(popsicle.plugins.parse('json'));
+    return popsicle.post(url, data).use(popsicle.plugins.parse('json')).use(status());
   }
 }
